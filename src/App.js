200b7b1685fa652a5352e9bfa55a59ee5fab4bd0
qinyao.mou@metlife.com <qinyao.mou@metlife.com>
2019-03-06 10:44:11 -0500

    correct dashboard layout

--- src/App.js
+++ src/App.js
@@ -58,150 +58,152 @@ class App extends Component {
             <img src={logo3} alt="okikiddo" className="footer-image"/>
           </div>
         </div>
-        <div className="center-content">
-          <div className="center-top-content">
-            <div className="status-card-container">
-              <p className="status-card-title">今日出勤</p>
-              <div className="status-card-fill">
-                <SimplePieChart />
-              </div>
-              <div className="status-card-footer">
-                <div className="card-footer-row">
-                  <div className="card-footer-general">
-                    <div className="card-status-label">
-                      <span className="card-status-block darkBlue"></span>
-                      <span>到校</span>
+        <div className="dashboard-content">
+          <div className="center-content">
+            <div className="center-top-content">
+              <div className="status-card-container">
+                <p className="status-card-title">今日出勤</p>
+                <div className="status-card-fill">
+                  <SimplePieChart />
+                </div>
+                <div className="status-card-footer">
+                  <div className="card-footer-row">
+                    <div className="card-footer-general">
+                      <div className="card-status-label">
+                        <span className="card-status-block darkBlue"></span>
+                        <span>到校</span>
+                      </div>
+                      <div className="card-footer-number-block"><span className="card-footer-number">146</span>人</div>
                     </div>
-                    <div className="card-footer-number-block"><span className="card-footer-number">146</span>人</div>
-                  </div>
-                  <div className="card-footer-general">
-                    <div className="card-status-label">
-                      <span className="card-status-block lightBlue"></span>
-                      <span>迟到</span>
+                    <div className="card-footer-general">
+                      <div className="card-status-label">
+                        <span className="card-status-block lightBlue"></span>
+                        <span>迟到</span>
+                      </div>
+                      <div className="card-footer-number-block"><span className="card-footer-number">20</span>人</div>
                     </div>
-                    <div className="card-footer-number-block"><span className="card-footer-number">20</span>人</div>
-                  </div>
-                  <div className="card-footer-general">
-                    <div className="card-status-label">
-                      <span className="card-status-block orange"></span>
-                      <span>请假</span>
+                    <div className="card-footer-general">
+                      <div className="card-status-label">
+                        <span className="card-status-block orange"></span>
+                        <span>请假</span>
+                      </div>
+                      <div className="card-footer-number-block"><span className="card-footer-number">25</span>人</div>
                     </div>
-                    <div className="card-footer-number-block"><span className="card-footer-number">25</span>人</div>
-                  </div>
-                  <div className="card-footer-general">
-                    <div className="card-status-label">
-                      <span className="card-status-block gray"></span> 
-                      <span>未知</span>
+                    <div className="card-footer-general">
+                      <div className="card-status-label">
+                        <span className="card-status-block gray"></span> 
+                        <span>未知</span>
+                      </div>
+                      <div className="card-footer-number-block"><span className="card-footer-number">4</span>人</div>
                     </div>
-                    <div className="card-footer-number-block"><span className="card-footer-number">4</span>人</div>
                   </div>
                 </div>
               </div>
-            </div>
-            <div className="status-card-container">
-              <p className="status-card-title">晨检异常</p>
-              <div className="status-card-fill card-report-section">
-                <div>
-                  <label>2</label>
-                  <span>人</span>
-                </div>
-              </div>
-              <div className="status-card-footer">
-                <div className="card-footer-row">
-                  <div className="card-footer-left">
-                    <span>大1班</span>
-                    <span>李小明</span>
+              <div className="status-card-container">
+                <p className="status-card-title">晨检异常</p>
+                <div className="status-card-fill card-report-section">
+                  <div>
+                    <label>2</label>
+                    <span>人</span>
                   </div>
-                  <div className="card-footer-right">38.9C</div>
                 </div>
-                <div className="card-footer-row">
-                  <div className="card-footer-left">
-                    <span>小2班</span>
-                    <span>张小小</span>
-                  </div>
-                  <div className="card-footer-right">29.9C</div>
-                </div>
-              </div>
-            </div>
-            <div className="status-card-container">
-              <p className="status-card-title">收支情况</p>
-              <div className="status-card-fill">
-              </div>
-              <div className="status-card-footer">
-                <div className="card-footer-row">
+                <div className="status-card-footer">
+                  <div className="card-footer-row">
                     <div className="card-footer-left">
-                      <span>收入：</span>
+                      <span>大1班</span>
+                      <span>李小明</span>
                     </div>
-                  <div className="card-footer-right">$320,106</div>
-                </div>
-                <div className="card-footer-row">
-                  <div className="card-footer-left">
-                    <span>支出：</span>
+                    <div className="card-footer-right">38.9C</div>
                   </div>
-                  <div className="card-footer-right">$210,306</div>
+                  <div className="card-footer-row">
+                    <div className="card-footer-left">
+                      <span>小2班</span>
+                      <span>张小小</span>
+                    </div>
+                    <div className="card-footer-right">29.9C</div>
+                  </div>
+                </div>
+              </div>
+              <div className="status-card-container">
+                <p className="status-card-title">收支情况</p>
+                <div className="status-card-fill">
+                </div>
+                <div className="status-card-footer">
+                  <div className="card-footer-row">
+                      <div className="card-footer-left">
+                        <span>收入：</span>
+                      </div>
+                    <div className="card-footer-right">$320,106</div>
+                  </div>
+                  <div className="card-footer-row">
+                    <div className="card-footer-left">
+                      <span>支出：</span>
+                    </div>
+                    <div className="card-footer-right">$210,306</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="center-bottom-content">
+              <p className="school-track-title">今日出勤</p>
+              <div className="school-track-content">
+                <div className="school-track-single">
+                  <div className="school-track-bar">
+                    <span className="school-track-section one"></span>
+                    <span className="school-track-section two"></span>
+                    <span className="school-track-section three"></span>
+                    <span className="school-track-section four"></span>
+                  </div>
+                  <div className="school-track-class">学前一班</div>
+                  <div className="school-track-profile-icon">
+                    <FontAwesomeIcon icon={faUserCircle} />
+                  </div>
+                </div>
+                <div className="school-track-single">
+                  <div className="school-track-bar">
+                    <span className="school-track-section one"></span>
+                    <span className="school-track-section two"></span>
+                    <span className="school-track-section three"></span>
+                    <span className="school-track-section four"></span>
+                  </div>
+                  <div className="school-track-class">学前二班</div>
+                  <div className="school-track-profile-icon">
+                    <FontAwesomeIcon icon={faUserCircle} />
+                  </div>
+                </div>
+                <div className="school-track-single">
+                  <div className="school-track-bar">
+                    <span className="school-track-section one"></span>
+                    <span className="school-track-section two"></span>
+                    <span className="school-track-section three"></span>
+                    <span className="school-track-section four"></span>
+                  </div>
+                  <div className="school-track-class">大1班</div>
+                  <div className="school-track-profile-icon">
+                    <FontAwesomeIcon icon={faUserCircle} />
+                  </div>
+                </div>
+                <div className="school-track-single">
+                  <div className="school-track-bar">
+                    <span className="school-track-section one"></span>
+                    <span className="school-track-section two"></span>
+                    <span className="school-track-section three"></span>
+                    <span className="school-track-section four"></span>
+                  </div>
+                  <div className="school-track-class">大2班</div>
+                  <div className="school-track-profile-icon">
+                    <FontAwesomeIcon icon={faUserCircle} />
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-          <div className="center-bottom-content">
-            <p className="school-track-title">今日出勤</p>
-            <div className="school-track-content">
-              <div className="school-track-single">
-                <div className="school-track-bar">
-                  <span className="school-track-section one"></span>
-                  <span className="school-track-section two"></span>
-                  <span className="school-track-section three"></span>
-                  <span className="school-track-section four"></span>
-                </div>
-                <div className="school-track-class">学前一班</div>
-                <div className="school-track-profile-icon">
-                  <FontAwesomeIcon icon={faUserCircle} />
-                </div>
-              </div>
-              <div className="school-track-single">
-                <div className="school-track-bar">
-                  <span className="school-track-section one"></span>
-                  <span className="school-track-section two"></span>
-                  <span className="school-track-section three"></span>
-                  <span className="school-track-section four"></span>
-                </div>
-                <div className="school-track-class">学前二班</div>
-                <div className="school-track-profile-icon">
-                  <FontAwesomeIcon icon={faUserCircle} />
-                </div>
-              </div>
-              <div className="school-track-single">
-                <div className="school-track-bar">
-                  <span className="school-track-section one"></span>
-                  <span className="school-track-section two"></span>
-                  <span className="school-track-section three"></span>
-                  <span className="school-track-section four"></span>
-                </div>
-                <div className="school-track-class">大1班</div>
-                <div className="school-track-profile-icon">
-                  <FontAwesomeIcon icon={faUserCircle} />
-                </div>
-              </div>
-              <div className="school-track-single">
-                <div className="school-track-bar">
-                  <span className="school-track-section one"></span>
-                  <span className="school-track-section two"></span>
-                  <span className="school-track-section three"></span>
-                  <span className="school-track-section four"></span>
-                </div>
-                <div className="school-track-class">大2班</div>
-                <div className="school-track-profile-icon">
-                  <FontAwesomeIcon icon={faUserCircle} />
-                </div>
-              </div>
-            </div>
+          <div className="right-content">
+            <Calendar
+              value={new Date()}
+              locale="zh-CN"
+            />
           </div>
-        </div>
-        <div className="right-content">
-          <Calendar
-            value={new Date()}
-            locale="zh-CN"
-          />
         </div>
       </div>
     );
